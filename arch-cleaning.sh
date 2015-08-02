de2e82a67e5bac546eac5ca3086c5405c14d74b3
Augusto <augusto.codex@gmail.com>
2015-08-01 22:51:57 -0300

    Initial list of taks to improve performance finished

--- arch-cleaning.sh
+++ arch-cleaning.sh
@@ -1,5 +1,5 @@
 # Check if any systemd services have entered in a failed state
-systemctl --failed
+sudo systemctl --failed
 
 # Look for high priority errors in the systemd journal
 sudo journalctl -p 0..3 -xn
@@ -9,23 +9,28 @@ sudo journalctl -p 0..3 -xn
 # Make a backup of the original mirrorlist
 sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 
-# Optionally run the following sed line to uncomment every mirror on backup
+# Run the following sed line to uncomment every mirror on backup
 sudo sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
 
 # Make the rank and copy it to /etc/pacman.d/mirrorlist to be used
-sudo rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
+sudo rankmirrors -v -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
 
 # Refresh all package lists even if they are considered to be up to date
+# and updates the system
 sudo pacman -Syyu
 
-# Recursively search for orphaned packages and if found, removes them
-orphans() {
-  if [[ ! -n $(pacman -Qdt) ]]; then
+# Search for orphaned packages and if found, removes them recursively
+if [[ ! -n $(pacman -Qdt) ]]; then
     echo "No orphans to remove."
-  else
+else
     sudo pacman -Rns $(pacman -Qdtq)
-  fi
-}
+fi
 
-# Calling the function
-orphans
+# Removes all the cached packages that are not currently installed
+sudo pacman -Sc
+
+# Optimize pacman database of installed packages
+sudo pacman-optimize
+
+# Trim the filesystem for deleting bad blocks when using an SSD
+sudo fstrim /
